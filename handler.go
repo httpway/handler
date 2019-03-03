37af01e42df90f85a8fab5624c59e66bdef5edf2
Diego Bernardes <diego.bernardes@outlook.com>
2019-03-03 22:42:19 +0000

    Improve handler signature

--- handler.go
+++ handler.go
@@ -2,8 +2,9 @@ package handler
 
 import (
 	"context"
+	"fmt"
 	"net/http"
-	"net/http/httputil"
+	"time"
 )
 
 // Client has the information needed to work as a httpway plugin.
@@ -15,9 +16,13 @@ func (Client) Close(ctx context.Context) error {
 }
 
 // Default act as a reverse proxy.
-func (Client) Default(w http.ResponseWriter, r *http.Request) {
-	proxy := httputil.NewSingleHostReverseProxy(r.URL)
-	proxy.ServeHTTP(w, r)
+func (c Client) Default(next http.Handler) http.Handler {
+	fn := func(w http.ResponseWriter, r *http.Request) {
+		t := time.Now()
+		next.ServeHTTP(w, r)
+		fmt.Println(time.Since(t))
+	}
+	return http.HandlerFunc(fn)
 }
 
 // NewClient return a initialized client.
