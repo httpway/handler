497a99fd2272898cdfe99da088cc81b54dd63a93
Diego Bernardes <diego.bernardes@outlook.com>
2019-03-06 20:48:20 +0000

    Simplify not found handler

--- go.mod
+++ go.mod
@@ -1,5 +1,3 @@
 module github.com/httpway/handler
 
 go 1.12
-
-require github.com/pkg/errors v0.8.1
--- handler.go
+++ handler.go
@@ -2,14 +2,11 @@ package handler
 
 import (
 	"context"
-	"encoding/json"
 	"fmt"
 	"net/http"
 	"os"
 	"runtime/debug"
 	"time"
-
-	"github.com/pkg/errors"
 )
 
 // Client has the information needed to work as a httpway plugin.
@@ -47,22 +44,7 @@ func Panic(next http.Handler) http.Handler {
 
 // NotFound is invoked to handle not found errors.
 func (Client) NotFound(w http.ResponseWriter, r *http.Request) {
-	payload, err := json.Marshal(map[string]interface{}{
-		"error": map[string]interface{}{
-			"status": http.StatusNotFound,
-			"uri":    r.URL.String(),
-		},
-	})
-	if err != nil {
-		fmt.Println(errors.Wrap(err, "response marshal error").Error())
-		return
-	}
-
-	if _, err := w.Write(payload); err != nil {
-		fmt.Println(errors.Wrap(err, "payload response write error").Error())
-		return
-	}
-	w.WriteHeader(http.StatusNotFound)
+	http.NotFound(w, r)
 }
 
 // NewClient return a initialized client.
