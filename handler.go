b15e50780186c68c4b057e56be996a32cb096ee2
Diego Bernardes <diego.bernardes@outlook.com>
2019-03-24 12:45:26 +0000

    Accept config parameter during initialization

--- handler.go
+++ handler.go
@@ -4,13 +4,18 @@ import (
 	"context"
 	"fmt"
 	"net/http"
+	"net/url"
 	"os"
 	"runtime/debug"
 	"time"
+
+	"github.com/pkg/errors"
 )
 
 // Client has the information needed to work as a httpway plugin.
-type Client struct{}
+type Client struct {
+	url *url.URL
+}
 
 // Close the client.
 func (Client) Close(ctx context.Context) error {
@@ -18,11 +23,11 @@ func (Client) Close(ctx context.Context) error {
 }
 
 // Default is a sample HTTP handler that track the time of a request.
-func (Client) Default(next http.Handler) http.Handler {
+func (c Client) Default(next http.Handler) http.Handler {
 	fn := func(w http.ResponseWriter, r *http.Request) {
-		r.Host = "www.google.com"
-		r.URL.Host = "www.google.com"
-		r.URL.Scheme = "https"
+		r.Host = c.url.Host
+		r.URL.Host = c.url.Host
+		r.URL.Scheme = c.url.Scheme
 
 		t := time.Now()
 		next.ServeHTTP(w, r)
@@ -51,7 +56,26 @@ func (Client) NotFound(w http.ResponseWriter, r *http.Request) {
 	http.NotFound(w, r)
 }
 
+func (c *Client) init(config map[string]interface{}) error {
+	rawHost, ok := config["host"].(string)
+	if !ok {
+		return errors.New("casting host to string error")
+	}
+
+	var err error
+	c.url, err = url.Parse(rawHost)
+	if err != nil {
+		return errors.Wrapf(err, "parse url '%s' error", rawHost)
+	}
+
+	return nil
+}
+
 // NewClient return a initialized client.
-func NewClient() (Client, error) {
-	return Client{}, nil
+func NewClient(config map[string]interface{}) (Client, error) {
+	var c Client
+	if err := c.init(config); err != nil {
+		return c, errors.Wrap(err, "initialization error")
+	}
+	return c, nil
 }
